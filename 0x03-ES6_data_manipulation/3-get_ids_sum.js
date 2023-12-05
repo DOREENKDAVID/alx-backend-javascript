fa5e877fce4e1f072364e4c513ba170bf38370f2
DOREENKDAVID <doreenkinya153@gmail.com>
2023-12-05 13:42:03 -0800

    data structures and data manipulation

--- 0x03-ES6_data_manipulation/3-get_ids_sum.js
+++ 0x03-ES6_data_manipulation/3-get_ids_sum.js
@@ -1,4 +1,4 @@
-//function getStudentIdsSum that returns the sum of all the student ids.
+// function getStudentIdsSum that returns the sum of all the student ids.
 
 export default function getStudentIdsSum(lst) {
   return lst.reduce((count, currentId) => count + currentId.id, 0);
