d645fd73b144c1c996a020920d4311fc35031bbd
DOREENKDAVID <doreenkinya153@gmail.com>
2024-02-02 14:43:20 -0800

    NodeJS Basics

---
+++ 0x05-Node_JS_basic/0-console.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+
+/*create a function named displayMessage that prints in STDOUT the string argument.*/
+const displayMessage = (msg) => {
+	console.log(msg);
+};
+
+module.exports = displayMessage;
